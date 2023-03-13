e8a4e0ad3a86e610411bef5b004783bfa8554c3f
Ashley <ashley.horn15@yahoo.com>
2023-03-13 16:11:14 -0500

    Add project files.

---
+++ RestaurantMenuStudio/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestaurantMenuStudio;
+
+namespace RestaurantMenuStudio
+{
+
+    public class Menu
+    {
+        public List<MenuItems> Items { get; set; }
+        DateTime ADate { get; set; }
+
+        public Menu(DateTime aDate, List<MenuItems> items)
+        {
+            Items = items;
+            ADate = aDate;
+        }
+    }
+}
